1e075f5b7c8c2e7e87d733e9bf637ae5268b4041
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-09-02 20:16:20 +0200

    sommit system test

--- blog/tests/system/app_test.py
+++ blog/tests/system/app_test.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from unittest.mock import patch
 import app
 from blog import Blog
+from post import Post
 
 
 class AppTest(TestCase):
@@ -32,4 +33,34 @@ class AppTest(TestCase):
             self.assertIsNotNone(app.blogs.get('Test'))
 
     def test_ask_read_blog(self):
+        blog = Blog('Test', 'Test Author')
+        app.blogs = {'Test': blog}
+        with patch('builtins.input', return_value='Test'):
+            with patch('app.print_posts') as mocked_print_posts:
+                app.ask_read_blog()
+
+                mocked_print_posts.assert_called_with(blog)
+
+
+    def test_print_posts(self):
+        blog = Blog('Test', 'Test Author')
+        blog.create_post('Test Post', 'Test Content')
+        app.blogs = {'Test': blog}
+
+        with patch('app.print_post') as mocked_print_post:
+            app.print_posts(blog)
+
+            mocked_print_post.assert_called_with(blog.posts[0])
+
+    def test_print_post(self):
+        post = Post('Post title', 'Post content')
+        expected_print = '''
+--- Post title ---
+Post content
+'''
+        with patch('builtins.print') as mocked_print:
+            app.print_post(post)
+
+            mocked_print.assert_called_with(expected_print)
+
 
