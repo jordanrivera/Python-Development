d36944ff703150649a96f769fdd24b3a638f727b
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-09-06 00:28:48 +0200

    intergration test

---
+++ starter_code/tests/intergration/models/test_item.py
@@ -0,0 +1,17 @@
+from models.item import ItemModel
+from tests.base_test import BaseTest
+
+class ItemTest(BaseTest):
+    def test_crud(self):
+        with self.app_context():
+            item = ItemModel('test', 19.99)
+
+            self.assertIsNone(ItemModel.find_by_name('test'))
+
+            item.save_to_db()
+
+            self.assertIsNotNone(ItemModel.find_by_name('test'))
+
+            item.delete_from_db()
+
+            self.assertIsNone(ItemModel.find_by_name('test'))
