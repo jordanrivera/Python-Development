92e51af7baabae9769699b856cfddceb3849b671
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-09-07 16:27:43 +0200

    add security login

---
+++ starter_code3/security.py
@@ -0,0 +1,28 @@
+from werkzeug.security import safe_str_cmp
+from models.user import UserModel
+
+def authenticate(username, password):
+    """
+    Function that gets called when a user calls the /auth endpoint
+    with their username and password.
+    :param username: User's username in string format.
+    :param password: User's un-encrypted password in  string format.
+    :return: a UserModel object if authentification was succesful, None otherwise.
+    """
+    user = UserModel.find_by_username(username)
+    if user and safe_str_cmp (user.password, password):
+        return user
+
+def identity(payload):
+    """
+    Function that gets called when user has already authentification, and Flask-JWT
+    verified their authorization header is correct.
+    :param payload: A dictionary with 'identity' key, which is the user id.
+    :return: A UserModel object
+    """
+    user_id = payload['identify']
+    return UserModel.find_by_id()
+
+
+
+
