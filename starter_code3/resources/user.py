a7492fc2d6c26dedf31b0c1d35a672ec0fcda001
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-09-08 12:01:28 +0200

    user test

--- starter_code3/resources/user.py
+++ starter_code3/resources/user.py
@@ -3,6 +3,10 @@ from models.user import UserModel
 
 
 class UserRegister(Resource):
+    """
+        This resource allows users to register by sending a
+        POST request with their username and password
+    """
     parser = reqparse.RequestParser()
     parser.add_argument('username', type=str, required=True,help="This field cannot be blank.")
     parser.add_argument('password', type=str, required=True, help="This field cannot be blank.")
