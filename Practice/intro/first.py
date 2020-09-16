f1ee80d8708828e2221809bfee3983a1fac5aeb1
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-09-16 14:08:50 +0200

    practice module

---
+++ Practice/intro/first.py
@@ -0,0 +1,20 @@
+age = 35        # integer
+PI = 3.14159    # float
+
+maths_operation = 1 + 3 * 4 / 2 - 2
+float_division = 12 / 3
+integer_division = 12 // 3      # two slash remove numbers after decimal
+print(float_division + maths_operation + integer_division)
+
+integer_division1 = 13 // 5
+print(integer_division1)
+remainder = 13 % 5              # modulus divide first and calculate the rest
+print(remainder)
+
+x = 37
+remainder2 = x % 2
+print(remainder2)
+
+row_number = 235
+result = row_number % 2
+print(result)
---
+++ Practice/intro/strings_excersice.py
@@ -0,0 +1,26 @@
+# Strings comments
+# Example comments
+"""
+This is also a comment
+"""
+
+my_string = "Hello, world!"
+print(my_string)
+
+string_with_quotes = "hello, it's me."
+another_with_quotes = 'He said "You ar amazing!" yesterday'
+multiline = """Hello, world.
+My name is Yordan. Welcome to my program.
+"""
+print(multiline)
+
+name = "Yordan"
+greeting = "Hello " + name
+print(greeting)
+
+age = 34
+ages_as_str = str(age)
+print("You are " + ages_as_str)
+
+
+
