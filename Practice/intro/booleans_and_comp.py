cdfda882a6d39f8bd12c442bdbc00bbf2766c1b9
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-09-19 23:35:22 +0200

    booleans practice

--- Practice/intro/booleans_and_comp.py
+++ Practice/intro/booleans_and_comp.py
@@ -8,5 +8,17 @@ my_number = 5
 user_number = int(input("Enter a number: "))
 
 matches = my_number == user_number
-
 print(f"You got it right: {matches}.")
+
+age = int(input("Enter your age: "))
+can_learn_programming = age > 0 and age < 150
+print(f"you can learn programming: {can_learn_programming}.")
+
+age = int(input("Enter your age: "))
+usually_working = age < 18 or age > 65
+print(f"At {age}, you are usually working: {usually_working}.")
+
+print(bool(35))
+print(bool(""))
+
+
