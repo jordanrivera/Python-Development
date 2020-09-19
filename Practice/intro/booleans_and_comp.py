6588e9344ad20c9fa175841e6ea250e83a12f981
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-09-19 09:12:30 +0200

    practice booleans

---
+++ Practice/intro/booleans_and_comp.py
@@ -0,0 +1,9 @@
+age = 20
+is_over_age = age >= 18
+is_under_age = age <= 18
+is_twenty = age == 20
+print(is_over_age)
+
+my_number = 5
+user_number = int(input("Enter a number: "))
+print(my_number == user_number)
