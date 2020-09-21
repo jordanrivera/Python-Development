57639d460d7db3055c378d78ed1ce4e47ab0f185
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-09-21 18:43:06 +0200

    commit

---
+++ Practice/fundamentals/while_loops.py
@@ -0,0 +1,8 @@
+# while loops
+user_input = input("Do you wish to run the program? (yes/no): ")
+
+while user_input == "yes":
+    print("We're running!")
+    user_input = input("Do you wish to run the program? (yes/no: ")
+
+print("we stopped running.")
