4bd7b01f1188529e5d1eb464b8bf1aae0055033f
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-09-21 17:24:19 +0200

    commit

--- Practice/fundamentals/if_statemensts_en.py
+++ Practice/fundamentals/if_statemensts_en.py
@@ -1,5 +1,28 @@
+# If statements
+
 friend = "Rolf"
 user_name = input("Enter your name: ")
 
-if True:
+if user_name == friend:
     print("Hello, friend")
+else:
+    print("Hello, stranger")
+# Bool of a empty / string
+name = input("Enter your name: ")
+print(bool(name))
+if name:
+    print("We know your name")
+
+# if statement iterate in an list
+friends = ["Rolf", "Bob", "Anne"]
+family = ["Jen", "Charlie"]
+
+user_name = input("Enter your name: ")
+
+if user_name in friends:
+    print("Hello, friend")
+elif user_name in family:
+    print("Hello, family!")
+else:
+    print("I don't know you.")
+
