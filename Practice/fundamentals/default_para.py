1f428bff26c44abff629082915a9a106bbe726ac
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-09-24 17:04:02 +0200

    commit

--- Practice/fundamentals/default_para.py
+++ Practice/fundamentals/default_para.py
@@ -6,3 +6,24 @@ def add(x, y=3):
 
 
 print(add(x=5, y=6))
+# change the space between default parameters
+print(1, 2, 3, 4, 5, sep=" - ")
+
+default_y = 3
+
+
+def add(x, y=default_y):
+    total = x + y
+    print(total)
+
+add(2)
+
+def hi():
+    print("Hellop")
+def greet():
+    return "Hellok"
+x = hi()
+y = greet()
+
+
+
