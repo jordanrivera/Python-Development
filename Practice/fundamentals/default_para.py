25496f4dd7b691ac89d16093cf45cd1b03458579
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-09-24 16:41:10 +0200

    commit

---
+++ Practice/fundamentals/default_para.py
@@ -0,0 +1,8 @@
+# default parameter functions
+
+def add(x, y=3):
+    total = x + y
+    return total
+
+
+print(add(x=5, y=6))
--- Practice/fundamentals/return_values.py
+++ Practice/fundamentals/return_values.py
@@ -1,3 +1,4 @@
+# return in functions
 cars = [
     {"make": "Ford", "model": "Fiesta", "mileage": 23000, "fuel_consumed": 460},
     {"make": "Ford", "model": "Focus", "mileage": 17000, "fuel_consumed": 350},
@@ -5,12 +6,37 @@ cars = [
     {"make": "Mini", "model": "Cooper", "mileage": 31000, "fuel_consumed": 235},
 ]
 
+
 def calculate_mpg(car):
     mpg = car["mileage"] / car["fuel_consumed"]
+    return mpg
+
+
+def car_name(car):
     name = f"{car['make']} {car['model']}"
+    return name
+
+
+def print_car_info(car):
+    name = car_name(car)
+    mpg = calculate_mpg(car)
+
     print(f"{name} does {mpg} miles per gallon.")
 
 
 for car in cars:
-    calculate_mpg(car)
+   print_car_info(car)
+
+
+# Functions can return multiples time
+def divide(x, y):
+    if y == 0:
+        return "You tried to divide by zero!"
+    else:
+        return x / y
+
+
+print(divide(10, 2))
+print(divide(6, 0))
+
 
