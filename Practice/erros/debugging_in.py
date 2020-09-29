01a6102ec145890f01c9a609fc6d5294133a4c78
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-09-30 00:21:38 +0200

    debugging

---
+++ Practice/erros/debugging_in.py
@@ -0,0 +1,26 @@
+class User:
+    def __init__(self, name, engagement):
+        self.name = name
+        self.engagement_metrics = engagement
+        self.score = 0
+
+    def __repr__(self):
+        return f'<User {self.name}>'
+
+
+def email_engaged_user(user):
+    try:
+        user.score = perform_calculation(user.engagement_metrics)
+    except KeyError:
+        print('Incorrect values provided to bur calculation function.')
+    else:
+        if user.score > 500:
+            send_engagement_notification(user)
+
+
+def perform_calculation(metrics):
+    return metrics['clicks'] * 5 + metrics['hits'] * 2
+
+
+def send_engagement_notification(user):
+    print(f'Notification sent to  {user}.')
