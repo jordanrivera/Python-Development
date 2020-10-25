4b65f0dd85a58ee5662883c9f3847c6c7c7e932d
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-10-25 21:14:36 +0100

    commit

--- file-handling/csv_read.py
+++ file-handling/csv_read.py
@@ -3,3 +3,12 @@ lines = file.readlines()
 file.close()
 
 lines = [line.strip() for line in lines[1:]]
+
+for line in lines:
+    person_data = line.split(',')
+    name = person_data[0]
+    age = person_data[1]
+    university = person_data[2]
+    degree = person_data[3]
+
+    
